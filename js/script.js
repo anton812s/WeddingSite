0581ead135b756ce6ae07a8799274dd302ae027b
Anton <anton812s@gmail.com>
2017-10-20 08:17:48 -0400

    added animsition

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    linkElement: 'header a',
+    inDuration: 1500,
+    outDuration: 500
+  });
+});
