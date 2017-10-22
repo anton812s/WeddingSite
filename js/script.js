501a037cf1d5139055f64deb0f6e28bf612fc3a0
Anton <anton812s@gmail.com>
2017-10-22 18:33:32 -0400

    possible swipe functionality

--- js/script.js
+++ js/script.js
@@ -6,4 +6,14 @@ $(document).ready(function() {
     inDuration: 1000,
     outDuration: 500
   });
+
+  $(function(){
+  // Bind the swipeHandler callback function to the swipe event on .animsition
+  $( ".animsition" ).on( "swipe", swipeHandler );
+
+  // Callback function references the event target and adds the 'swipe' class to it
+  function swipeHandler( event ){
+    $( ".location" ).trigger('click');
+  }
+});
 });
